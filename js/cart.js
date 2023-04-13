f59f25563a42d3fd9920c2106714438a2fd293fe
Pubudu Bandara <pubudubandara09@gmail.com>
2023-04-13 13:19:14 -0400

    cart update and remove

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var items = []
+    var totalAmount = 0.0;
     if(localStorage.getItem('items')){
         console.log("items found")
         items = JSON.parse(localStorage.getItem('items'))
@@ -9,21 +10,30 @@ $(document).ready(function(){
     for (var i = 0; i < items.length; i++) {
         var quantityId = items[i].name+'-quantity';
         var removeId = +items[i].name+'-btn';
+        totalAmount += items[i].price * items[i].quantity;
         str += '<tr class="item" id="'+items[i].name+'">' +
           '<td><h3>' + items[i].name +'</h3></td>'+
           '<td><img src = ' + items[i].img + '></td>' +
           '<td><input class="quantity" type="number" value = '+items[i].quantity+'></input></td>'+
           '<td><p>' + items[i].price +  '</p></td>'+
-          '<td><p>' + items[i].price * items[i].quantity+  '</p></td>'+
+          '<td><p>' + (items[i].price * items[i].quantity).toFixed(2)+  '</p></td>'+
           '<td><button class="remove-btn">Remove</button></td>'+
           '</tr>';
     }
     $(".cart").append(str);
 
+    $("#total-amount").html(totalAmount.toFixed(2));
+
     var removeBtns = document.getElementsByClassName('remove-btn')
     for (var i = 0; i < removeBtns.length; i++) {
         var button = removeBtns[i]
         button.addEventListener('click', removeCartItem)
+    }
+
+    var quantitys = document.getElementsByClassName('quantity')
+    for (var i = 0; i < quantitys.length; i++) {
+        var input = quantitys[i]
+        input.addEventListener('change', quantityChanged)
     }
 })
 
@@ -38,16 +48,10 @@ function removeCartItem(event) {
             if (rowElement.id == items[j].name){
                 items.splice(j, 1);
                 console.log("item found" + j)
-                // cart.splice(cart[j],1);
             }
         }
 
         console.log(items)
-
-        // let index = cart.indexOf(rowElement.id)
-        // if (index > -1) {
-        //     items = items.splice(index, 1);
-        // }
         localStorage.setItem('items',JSON.stringify(items))
         window.alert("Item Removed")
 
@@ -56,44 +60,54 @@ function removeCartItem(event) {
         window.alert("Item Already removed")
     }
     buttonClicked.parentElement.parentElement.remove()
-    //updateCartTotal()
+    updateCartTotal()
 }
 
-var reload = function(){
-    var items = []
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    console.log("input changed")
+
+    var priceElement = input.parentElement.nextElementSibling.firstChild;
+    var price = +priceElement.innerHTML;
+    price = parseFloat(price);
+    var totalElement = priceElement.parentElement.nextElementSibling.firstChild;
+    totalElement.innerHTML = (price*input.value).toFixed(2);
+
+    var rowElement = input.parentElement.parentElement;
+
     if(localStorage.getItem('items')){
-        console.log("items found")
         items = JSON.parse(localStorage.getItem('items'))
+
+        for (let j = 0; j < items.length; j++){
+            if (rowElement.id == items[j].name){
+                items[j].quantity = input.value;
+            }
+        }
+
+        console.log(items);
+        localStorage.setItem('items',JSON.stringify(items));
+        window.alert("Item Updated");
+
+    }
+    else{
+        window.alert("Item not avaialable Refresh!");
     }
 
-    var str = '';
-    for (var i = 0; i < items.length; i++) {
-        var quantityId = items[i].name+'-quantity';
-        var removeId = +items[i].name+'-btn';
-        str += '<tr>' +
-          '<td><h3>' + items[i].name +'</h3></td>'+
-          '<td><img src = ' + items[i].img + '></td>' +
-          '<td><input id="'+items[i].name+'-quantity" type="number" value = '+items[i].quantity+'></input></td>'+
-          '<td><p>' + items[i].price +  '</p></td>'+
-          '<td><p>' + items[i].price * items[i].quantity+  '</p></td>'+
-          '<td><button id="'+items[i].name+'-btn" >Remove</button></td>'+
-          '</tr>';
-
-        //   $('#'+quantityId).bind('keyup mouseup', function () {
-        //     for (var i = 0; i < items.length; i++) {
-        //         if(items[i].name = items[i].name){
-        //             items[i].quantity = $('#'+quantityId).value;
-        //             reload();
-        //         }
-        //         }       
-        //     });
-        // $('#'+quantityId).on('click',function(){
-        //     items = items.splice(i, 1);
-        //     localStorage.setItem('items',JSON.stringify(items));
-        //     reload();
-        // });
-    }
-    // str+='</div>';
-
-    $(".cart").append(str);
+    updateCartTotal();
 }
+
+function updateCartTotal(){
+    var totalAmount = 0.0;
+    if(localStorage.getItem('items')){
+        items = JSON.parse(localStorage.getItem('items'))
+        for (var i = 0; i < items.length; i++) {
+            totalAmount += items[i].price * items[i].quantity;
+        }
+
+        $("#total-amount").html(totalAmount.toFixed(2));
+    }
+}
+
