18ffd0f37ac8cd1af26d9d30577a0e43254a1b36
Pubudu Bandara <pubudubandara09@gmail.com>
2023-04-13 11:06:24 -0400

    removing from the cart

--- js/addToCart.js
+++ js/addToCart.js
@@ -5,6 +5,12 @@ $(document).ready(function(){
         console.log("clicked")
         if(localStorage.getItem('items')){
             items = JSON.parse(localStorage.getItem('items'))
+            for (let j = 0; j < items.length; j++){
+                if (items[j].name == 'Nike Pro'){
+                    window.alert("item already available in the cart");
+                    return;
+                }
+            }
             items.push({name:'Nike Pro',price: '59.99', img:'media/images/men6.jpg', quantity:'1'})
             localStorage.setItem('items',JSON.stringify(items))
            }
@@ -12,12 +18,19 @@ $(document).ready(function(){
             items.push({name:'Nike Pro',price: '59.99', img:'media/images/men6.jpg',quantity:'1'})
             localStorage.setItem('items',JSON.stringify([{name:'Nike Pro',price: '59.99', img:'media/images/men6.jpg',quantity:'1'}]))
            }
+        window.alert("Item Added")
     })
 
     $('#air-jordan').on('click',function(){
         console.log("clicked")
         if(localStorage.getItem('items')){
             items = JSON.parse(localStorage.getItem('items'))
+            for (let j = 0; j < items.length; j++){
+                if (items[j].name == 'Air Jordan'){
+                    window.alert("item already available in the cart");
+                    return;
+                }
+            }
             items.push({name:'Air Jordan',price: '49.99', img:'media/images/men3.jpg',quantity:'1'})
             localStorage.setItem('items',JSON.stringify(items))
            }
@@ -25,12 +38,19 @@ $(document).ready(function(){
             items.push({name:'Air Jordan',price: '49.99', img:'media/images/men3.jpg',quantity:'1'})
             localStorage.setItem('items',JSON.stringify([{name:'Air Jordan',price: '49.99', img:'media/images/men3.jpg',quantity:'1'}]))
            }
+        window.alert("Item Added")
     })
 
     $('#nike-blazers').on('click',function(){
         console.log("clicked")
         if(localStorage.getItem('items')){
             items = JSON.parse(localStorage.getItem('items'))
+            for (let j = 0; j < items.length; j++){
+                if (items[j].name == 'Nike Blazers'){
+                    window.alert("item already available in the cart");
+                    return;
+                }
+            }
             items.push({name:'Nike Blazers',price: '99.99', img:'media/images/men5.jpg', quantity:'1'})
             localStorage.setItem('items',JSON.stringify(items))
            }
@@ -38,12 +58,19 @@ $(document).ready(function(){
             items.push({name:'Nike Blazers',price: '99.99', img:'media/images/men5.jpg',quantity:'1'})
             localStorage.setItem('items',JSON.stringify([{name:'Nike Blazers',price: '99.99', img:'media/images/men5.jpg',quantity:'1'}]))
            }
+        window.alert("Item Added")
     })
 
     $('#air-force-1').on('click',function(){
         console.log("clicked")
         if(localStorage.getItem('items')){
             items = JSON.parse(localStorage.getItem('items'))
+            for (let j = 0; j < items.length; j++){
+                if (items[j].name == 'Air Force 1'){
+                    window.alert("item already available in the cart");
+                    return;
+                }
+            }
             items.push({name:'Air Force 1',price: '129.99', img:'media/images/men4.jpg', quantity:'1'})
             localStorage.setItem('items',JSON.stringify(items))
            }
@@ -51,5 +78,6 @@ $(document).ready(function(){
             items.push({name:'Air Force 1',price: '129.99', img:'media/images/men4.jpg',quantity:'1'})
             localStorage.setItem('items',JSON.stringify([{name:'Air Force 1',price: '129.99', img:'media/images/men4.jpg',quantity:'1'}]))
            }
+        window.alert("Item Added")
     })
 })
--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,63 @@
 $(document).ready(function(){
-    reload();
+    var items = []
+    if(localStorage.getItem('items')){
+        console.log("items found")
+        items = JSON.parse(localStorage.getItem('items'))
+    }
+
+    var str = '';
+    for (var i = 0; i < items.length; i++) {
+        var quantityId = items[i].name+'-quantity';
+        var removeId = +items[i].name+'-btn';
+        str += '<tr class="item" id="'+items[i].name+'">' +
+          '<td><h3>' + items[i].name +'</h3></td>'+
+          '<td><img src = ' + items[i].img + '></td>' +
+          '<td><input class="quantity" type="number" value = '+items[i].quantity+'></input></td>'+
+          '<td><p>' + items[i].price +  '</p></td>'+
+          '<td><p>' + items[i].price * items[i].quantity+  '</p></td>'+
+          '<td><button class="remove-btn">Remove</button></td>'+
+          '</tr>';
+    }
+    $(".cart").append(str);
+
+    var removeBtns = document.getElementsByClassName('remove-btn')
+    for (var i = 0; i < removeBtns.length; i++) {
+        var button = removeBtns[i]
+        button.addEventListener('click', removeCartItem)
+    }
 })
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    var rowElement = buttonClicked.parentElement.parentElement;
+
+    if(localStorage.getItem('items')){
+        items = JSON.parse(localStorage.getItem('items'))
+
+        for (let j = 0; j < items.length; j++){
+            if (rowElement.id == items[j].name){
+                items.splice(j, 1);
+                console.log("item found" + j)
+                // cart.splice(cart[j],1);
+            }
+        }
+
+        console.log(items)
+
+        // let index = cart.indexOf(rowElement.id)
+        // if (index > -1) {
+        //     items = items.splice(index, 1);
+        // }
+        localStorage.setItem('items',JSON.stringify(items))
+        window.alert("Item Removed")
+
+    }
+    else{
+        window.alert("Item Already removed")
+    }
+    buttonClicked.parentElement.parentElement.remove()
+    //updateCartTotal()
+}
 
 var reload = function(){
     var items = []
@@ -13,29 +70,30 @@ var reload = function(){
     for (var i = 0; i < items.length; i++) {
         var quantityId = items[i].name+'-quantity';
         var removeId = +items[i].name+'-btn';
-        str += '<div class ="cart-items">' +
-          '<h3>' + items[i].name +'</h3>'+
-          '<img src = ' + items[i].img + '>' +
-          '<input id="'+items[i].name+'-quantity" type="number" value = '+items[i].quantity+'></input>'+
-          '<p>Price : ' + items[i].price +  '</p>'+
-          '<p>Item Total : ' + items[i].price * items[i].quantity+  '</p>'+
-          '<button id="'+items[i].name+'-btn" >Remove</button>'+
-          '</div>';
+        str += '<tr>' +
+          '<td><h3>' + items[i].name +'</h3></td>'+
+          '<td><img src = ' + items[i].img + '></td>' +
+          '<td><input id="'+items[i].name+'-quantity" type="number" value = '+items[i].quantity+'></input></td>'+
+          '<td><p>' + items[i].price +  '</p></td>'+
+          '<td><p>' + items[i].price * items[i].quantity+  '</p></td>'+
+          '<td><button id="'+items[i].name+'-btn" >Remove</button></td>'+
+          '</tr>';
 
-          $('#'+quantityId).bind('keyup mouseup', function () {
-            for (var i = 0; i < items.length; i++) {
-                if(items[i].name = items[i].name){
-                    items[i].quantity = $('#'+quantityId).value;
-                    reload();
-                }
-                }       
-            });
-        $('#'+quantityId).on('click',function(){
-            items = items.splice(i, 1);
-            localStorage.setItem('items',JSON.stringify(items));
-            reload();
-        });
-      }
+        //   $('#'+quantityId).bind('keyup mouseup', function () {
+        //     for (var i = 0; i < items.length; i++) {
+        //         if(items[i].name = items[i].name){
+        //             items[i].quantity = $('#'+quantityId).value;
+        //             reload();
+        //         }
+        //         }       
+        //     });
+        // $('#'+quantityId).on('click',function(){
+        //     items = items.splice(i, 1);
+        //     localStorage.setItem('items',JSON.stringify(items));
+        //     reload();
+        // });
+    }
+    // str+='</div>';
 
-      $(".cart").append(str);
+    $(".cart").append(str);
 }
